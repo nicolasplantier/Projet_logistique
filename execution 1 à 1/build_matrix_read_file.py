fabc3f0407dbae8ef9977cb9b7fa640c81eb45ca
nicolasplantier <91543973+nicolasplantier@users.noreply.github.com>
2022-07-12 18:12:55 +0100

    Delete execution 1 à 1 directory

--- execution 1 à 1/build_matrix_read_file.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-with open ('Matrice des noeuds pour le cluster', 'rb') as temp:
-    items = pickle.load(temp)
-
-# with open ('Matrice des temps pour le cluster', 'rb') as temp:
-#     items_temps = pickle.load(temp)
-
-print(items)
--- execution 1 à 1/find_all_nodes_read_file.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open ('Liste des dictionnaires des noeuds dans le cluster 2, découpé en sous-clusters', 'rb') as temp:
-    items = pickle.load(temp)
-
-print(items)
